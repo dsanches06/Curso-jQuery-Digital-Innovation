a4187b752db827ab93b6c3b2db73a2a0b9a8911c
Danilson Sanches <dsanches06@outlook.com>
2021-01-13 18:57:31 +0000

    uso de jquery para manipular elemento HTML atraves de id ou class e ajax para requisiçoes json

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,22 @@
-alert("Funcionou");
+function consultaCep() {
+  let cep = document.getElementById("cep").value;
+  console.log("CEP: " + cep);
+  $.ajax({
+    url: "https://viacep.com.br/ws/" + cep + "/json/",
+    type: GET,
+    success: function (response) {
+      console.log(response);
+      /* utilização de jquery com propriedades htlm */
+      $("#cepValue").html(response.cep);
+      $("#logradouro").html(response.logradouro);
+      $("#complemento").html(response.complemento);
+      $("#bairro").html(response.bairro);
+      $("#localidade").html(response.localidade);
+      $("#uf").html(response.uf);
+      $("#ibge").html(response.ibge);
+      $("#gia").html(response.gia);
+      $("#ddd").html(response.ddd);
+      $("#siafi").html(response.siafi);
+    },
+  });
+}
